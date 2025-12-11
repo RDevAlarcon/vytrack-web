b89eba0e40ea2a8f1d97fc83756ee73b05b3a8a0
Ricardo Alarcón <alarconcastroricardo@gmail.com>
2025-12-11 08:52:08 -0300

    chore(tracking map): use cleaned truck icons and enlarge marker

--- src/components/map/FleetMap.tsx
+++ src/components/map/FleetMap.tsx
@@ -15,7 +15,7 @@ const Polyline = dynamic(() => import('react-leaflet').then((m) => m.Polyline), 
 import L from 'leaflet';
 
 const truckIcon = new L.Icon({
-  iconUrl: '/icons/truck3.png',
+  iconUrl: '/icons/truck5.png',
   iconSize: [32, 20],
   iconAnchor: [16, 20],
   popupAnchor: [0, -20],
